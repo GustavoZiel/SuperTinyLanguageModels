9e0fda1074d49c968515db35a7f01574e829e56c
Ziel <gugagribeiro@hotmail.com>
2025-08-25 21:50:29 -0300

    Foyrth commit - Changing to Ruff

--- train.py
+++ train.py
@@ -1,25 +1,22 @@
-"""
-The main training code
-"""
+"""The main training code"""
+
 import os
 
 import hydra
+import torch
+import torch.multiprocessing as mp
+from torch.distributed import destroy_process_group
 
 from models.build_models import build_model
-from trainers.build_trainers import build_trainer, ddp_setup
-from trainers import base_trainer
-from trainers.utils import create_folder_structure, init_print_override, restore_print_override
 from models.utils import print_model_stats
-
-import torch
-from torch.distributed import destroy_process_group
-import torch.multiprocessing as mp
+from trainers import base_trainer
+from trainers.build_trainers import build_trainer, ddp_setup
 from trainers.prepare import prepare_data
+from trainers.utils import create_folder_structure, init_print_override, restore_print_override
+
 
 def ddp_main(rank, world_size, cfg):
-    """
-    Main function for distributed training
-    """
+    """Main function for distributed training"""
     os.environ["GLOBAL_RANK"] = str(rank)
 
     original_print = init_print_override()
@@ -34,15 +31,11 @@ def ddp_main(rank, world_size, cfg):
         print(f"Rank{rank} Model built")
         print_model_stats(model)
         # load the relevant trainer
-        trainer: base_trainer.BaseTrainer = build_trainer(
-            cfg=cfg,
-            model=model,
-            gpu_id=rank
-        )
+        trainer: base_trainer.BaseTrainer = build_trainer(cfg=cfg, model=model, gpu_id=rank)
         print(f"Rank{rank} Trainer built")
         # train the model
         trainer.train()
-    
+
     finally:
         # clean up
         destroy_process_group()
@@ -50,10 +43,9 @@ def ddp_main(rank, world_size, cfg):
         # restore the print function
         restore_print_override(original_print)
 
+
 def basic_main(cfg):
-    """
-    Main function for single GPU training
-    """
+    """Main function for single GPU training"""
     model = build_model(model_cfg=cfg["model"])
     model.to(cfg["general"]["device"])
     model.train()
@@ -62,7 +54,7 @@ def basic_main(cfg):
     trainer = build_trainer(
         cfg=cfg,
         model=model,
-        gpu_id=None # disables DDP
+        gpu_id=None,  # disables DDP
     )
 
     # train the model
@@ -71,17 +63,18 @@ def basic_main(cfg):
 
 @hydra.main(config_path="configs", config_name="train")
 def main(cfg):
+    """Main function."""
     world_size = torch.cuda.device_count()
-    
+
     if "full_configs" in cfg:
         cfg = cfg["full_configs"]
     cfg["general"]["paths"]["data_dir"] = hydra.utils.to_absolute_path(
         cfg["general"]["paths"]["data_dir"]
-    ) # must be done before multiprocessing or else the path is wrong?
+    )  # must be done before multiprocessing or else the path is wrong?
 
     create_folder_structure(path_config=cfg["general"]["paths"])
 
-    # process data 
+    # process data
     prepare_data(cfg)
 
     if world_size <= 1:
