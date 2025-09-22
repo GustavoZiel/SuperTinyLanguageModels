8558c6d2495e99609fbb52a43919873d704a871f
Ziel <gugagribeiro@hotmail.com>
2025-09-22 10:34:27 +0800

    Improve generation code

--- generate.py
+++ generate.py
@@ -24,13 +24,26 @@ def main(cfg):
 
     generator = StandardGenerator(model=model, generate_cfg=cfg["generator"])
 
-    while True:
-        input_text = input("Enter the input text: ")
-        if input_text.lower() in ["exit", "quit"]:
-            print("Exiting...")
-            break
-        generated_text = generator.default_generate(input_text=input_text)
-        print(f"{model_filename}: {generated_text[0]}")
+    if "input_prompts" in cfg["generator"]:
+        logger.info("Prompting model from config file input prompts.")
+        generated = ""
+        for input_num, input_text in enumerate(cfg["generator"]["input_prompts"], start=1):
+            generated_text = generator.default_generate(input_text=input_text)
+            generated += (
+                "=" * 30 + f"\n\nQuestion {input_num}\n\n"
+                f"Prompt:\n{input_text}\n\n"
+                f"Generated:\n{generated_text[0]}\n\n"
+            )
+        print(generated)
+    else:
+        logger.info("Prompting model from user input. Type 'exit' or 'quit' to stop.")
+        while True:
+            input_text = input("Enter the input text: ")
+            if input_text.lower() in ["exit", "quit"]:
+                print("Exiting...")
+                break
+            generated_text = generator.default_generate(input_text=input_text)
+            print(f"{model_filename}: {generated_text[0]}")
 
 
 if __name__ == "__main__":
