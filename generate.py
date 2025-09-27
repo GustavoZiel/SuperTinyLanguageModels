1509db9a22b81e088c4d076010382f20d4c5f197
Ziel <gugagribeiro@hotmail.com>
2025-09-27 14:18:49 +0800

    Fixing generator code vast_ai

--- generate.py
+++ generate.py
@@ -5,7 +5,7 @@ import torch
 
 from models.build_models import build_model
 from models.generator import StandardGenerator
-from utils.logger import get_logger
+from utils import get_logger
 
 logger = get_logger(__name__)
 
@@ -21,6 +21,7 @@ def main(cfg):
 
     # load checkpoint from the path
     model = build_model(checkpoint=torch.load(model_filename_absolute_path, weights_only=False))
+    model.eval()
 
     generator = StandardGenerator(model=model, generate_cfg=cfg["generator"])
 
@@ -28,22 +29,32 @@ def main(cfg):
         logger.info("Prompting model from config file input prompts.")
         generated = ""
         for input_num, input_text in enumerate(cfg["generator"]["input_prompts"], start=1):
-            generated_text = generator.default_generate(input_text=input_text)
+            generated_text, messages = generator.default_generate(input_text=input_text)
             generated += (
                 "=" * 30 + f"\n\nQuestion {input_num}\n\n"
                 f"Prompt:\n{input_text}\n\n"
                 f"Generated:\n{generated_text[0]}\n\n"
             )
-        print(generated)
+            for message in messages[:1]:
+                generated += message + "\n"
+            generated += "=" * 30 + "\n\n"
+        logger.info(generated)
     else:
         logger.info("Prompting model from user input. Type 'exit' or 'quit' to stop.")
+        generated = ""
         while True:
             input_text = input("Enter the input text: ")
             if input_text.lower() in ["exit", "quit"]:
-                print("Exiting...")
+                logger.info("Exiting...")
                 break
-            generated_text = generator.default_generate(input_text=input_text)
-            print(f"{model_filename}: {generated_text[0]}")
+            generated_text, messages = generator.default_generate(input_text=input_text)
+            generated += (
+                "=" * 30 + f"\n\nPrompt:\n{input_text}\n\nGenerated:\n{generated_text[0]}\n\n"
+            )
+            for message in messages[:1]:
+                generated += message + "\n"
+            generated += "=" * 30 + "\n\n"
+            logger.info(generated)
 
 
 if __name__ == "__main__":
